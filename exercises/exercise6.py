a7f618f2215b933f3460da54f370b83ac695cdfe
aeloh1m <melowgz@gmail.com>
2022-05-12 18:44:47 -0300

    Pusheo un par que ya había realizado

--- exercises/exercise6.py
+++ exercises/exercise6.py
@@ -1,5 +1,6 @@
 """Type, Comprensión de Listas, Sorted y Filter."""
 
+from re import I
 from typing import List, Union
 
 
@@ -12,10 +13,20 @@ def numeros_al_final_basico(lista: List[Union[float, str]]) -> List[Union[float,
         - Utilizar la función type.
         - No utilizar índices.
     """
+    newList = []
 
+    for i in lista:
+        if type(i) == int:
+            newList.append(i)
+        if type(i) == str:
+            newList.insert(-6, i)
+            
+    return newList
+
+print(numeros_al_final_basico([3, "a", 1, "b", 10, "j"]))
 
 # NO MODIFICAR - INICIO
-assert numeros_al_final_basico([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]  # noqa: E501
+#assert numeros_al_final_basico([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]  # noqa: E501
 # NO MODIFICAR - FIN
 
 
@@ -29,7 +40,13 @@ def numeros_al_final_comprension(lista: List[Union[float, str]]) -> List[Union[f
         - No utilizar bucles.
         - Utilizar dos comprensiones de listas.
     """
-
+    newList = [i for i in lista if type(i) == str]
+   
+    newList_0 = [i for i in lista if type(i) == int]
+    
+    return newList + newList_0
+    
+print(numeros_al_final_comprension([3, "a", 1, "b", 10, "j"]))
 
 # NO MODIFICAR - INICIO
 assert numeros_al_final_comprension([3, "a", 1, "b", 10, "j"]) == ["a", "b", "j", 3, 1, 10]  # noqa: E501
